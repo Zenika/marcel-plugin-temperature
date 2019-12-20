64385670fae9e0a2ff4bcc168454de6e4831e4b8
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2019-12-20 11:16:36 +0100

    Firestore

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,36 @@
 class Marcelsius extends Marcel.Plugin {
   constructor() {
     super()
-    this.root = document.getElementById('root')
+    this.root = document.querySelector('#root')
+    this.temp = document.querySelector('#temp')
+    this.humidity = document.querySelector('#humidity')
+
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+      apiKey: "AIzaSyBcmQzvwiuXmOfQ4PX5n-Ic_OrwDK29v3U",
+      authDomain: "marcelsius.firebaseapp.com",
+      databaseURL: "https://marcelsius.firebaseio.com",
+      projectId: "marcelsius",
+      storageBucket: "marcelsius.appspot.com",
+      messagingSenderId: "354306015457",
+      appId: "1:354306015457:web:4b4a6a6ba11e009bfe77a0"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig)
+    this.db = firebase.firestore()
   }
 
   render() {
     const { stylesvar = {} } = this.props
+
+    this.db.collection("measures")
+      .orderBy("timestamp", "desc")
+      .limit(1)
+      .onSnapshot(data => {
+        const measure = data.docs[0].data()
+        this.temp.innerHTML = `${measure.temp.toLocaleString({ maximumFractionDigits: 1 })}°C`
+        this.humidity.innerHTML = `${measure.humidity.toLocaleString({ maximumFractionDigits: 1 })}%`
+      })
 
     // stylesvar is a special property containing the global media theme.
     // You should use it to have a consistent style accross all the media.
@@ -15,6 +40,3 @@ class Marcelsius extends Marcel.Plugin {
 }
 
 Marcel.init(Marcelsius)
-
-// uncomment this line to try the plugin in a browser :
-// Marcel.changeProps({ firstName: 'Marcel' })
